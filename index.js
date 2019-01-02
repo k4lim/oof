8e65479c2fa777678d0279dfd3e38331861da081
k4lim <35845133+k4lim@users.noreply.github.com>
2019-01-03 00:14:52 +0100

    Don't play sound if author is not in voice channel

--- index.js
+++ index.js
@@ -3,12 +3,13 @@ var bot = new Discord.Client();
 var isReady = true;
 
 bot.on('message', message => {
-  if (isReady && message.content === '.oof')
-  {
+  if (isReady && message.content === '.oof') {
+    var voiceChannel = message.member.voiceChannel;
+    if(!voiceChannel) {
+      return message.reply("Join a voice channel before using that command.");
+    }
   isReady = false;
-  var voiceChannel = message.member.voiceChannel;
-  voiceChannel.join().then(connection =>
-  {
+  voiceChannel.join().then(connection => {
      const dispatcher = connection.playFile('./oof.mp3');
      dispatcher.on("end", end => {
        voiceChannel.leave();
@@ -17,6 +18,5 @@ bot.on('message', message => {
    isReady = true;
   }
 });
-
 
 bot.login('NTMwMDU5NTIyNjYwNzYxNjIy.Dw7RFA.LfLwySFPa7CJla2zzvUF7yeUtc0');
